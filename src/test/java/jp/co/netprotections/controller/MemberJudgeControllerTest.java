26792f4fdbe1b3ee7bbaa3b073205cc5e003fcdf
take-np <tmiyazawa@netprotections.co.jp>
2019-07-11 01:55:38 +0900

    Ready for mvcTest

--- src/test/java/jp/co/netprotections/controller/MemberJudgeControllerTest.java
+++ src/test/java/jp/co/netprotections/controller/MemberJudgeControllerTest.java
@@ -1,8 +1,25 @@
 package jp.co.netprotections.controller;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 
 public class MemberJudgeControllerTest {
-
+	private MockMvc mockMvc;
+	
+	@Autowired
+	MemberJudgeController targetController;
+	
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(targetController).build();
+	}
+	
 }
