1c92efd539c10648715c3c41cd04a193d7d62991
take-np <tmiyazawa@netprotections.co.jp>
2019-10-23 22:55:28 +0900

    add skelton of ControllerTest

--- src/test/java/jp/co/netprotections/controller/MemberJudgeControllerTest.java
+++ src/test/java/jp/co/netprotections/controller/MemberJudgeControllerTest.java
@@ -1,5 +1,6 @@
 package jp.co.netprotections.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,24 +13,40 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+// JUnit上でもSpringのDI機能を利用するために、下のannotationを追加
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-
 public class MemberJudgeControllerTest {
 	private MockMvc mockMvc;
 	
+	// @Autowiredでテスト対象のクラスをDIコンテナに登録
 	@Autowired
 	MemberJudgeController targetController;
 	
+	// MockMvcBuilders.standaloneSetupで、springMVCの動作を再現するための準備	
+	// mockMvcインスタンスを利用して下層のリクエストを発生させ、テストを実行する
 	@Before
 	public void setup() {
 		mockMvc = MockMvcBuilders.standaloneSetup(targetController).build();
 	}
 	
+	
+	/**
+	 * レスポンスのHTTPステータスコードのテスト -> status 200
+	 * @return true
+	 * @throws Exception
+	 */
 	@Test
 	public void judgeMemberTest() throws Exception {
-		mockMvc.perform(post("judgemember"))
+		mockMvc.perform(post("/judgemember"))
 		.andExpect(status().isOk());
 		
 	}
+	
+	@Test
+	public void getTest() throws Exception {
+		mockMvc.perform(get("/judgemember"))
+		.andExpect(status().isNotFound());
+	}
+	
 }
