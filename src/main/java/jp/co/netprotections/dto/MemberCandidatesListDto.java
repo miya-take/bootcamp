49a30420dd1b2cc65dd736ad5f0d2901328d487c
take-np <tmiyazawa@netprotections.co.jp>
2019-12-05 21:28:25 +0900

    JavaDocを追加

--- src/main/java/jp/co/netprotections/dto/MemberCandidatesListDto.java
+++ src/main/java/jp/co/netprotections/dto/MemberCandidatesListDto.java
@@ -3,13 +3,25 @@ package jp.co.netprotections.dto;
 import java.util.List;
 
 
+/**
+ * @author t.miyazawa
+ * 入退社候補者リストのDTO
+ */
 public class MemberCandidatesListDto {
   private List<MemberJudgeRequestDto> memberCandidatesList;
 
+  /**
+   * 入退社候補者リストのgetter
+   * @return List<MemberJudgeRequestDto>
+   */
   public List<MemberJudgeRequestDto> getMemberCandidateList() {
     return this.memberCandidatesList;
   }
 
+  /**
+   * 入退社候補者リストのsetter
+   * @param memberCandidatesList
+   */
   public void setMemberCandidatesList(List<MemberJudgeRequestDto> memberCandidatesList) {
     this.memberCandidatesList = memberCandidatesList;
   }
--- src/main/java/jp/co/netprotections/dto/MemberJudgeResponseDto.java
+++ src/main/java/jp/co/netprotections/dto/MemberJudgeResponseDto.java
@@ -1,22 +1,12 @@
 package jp.co.netprotections.dto;
 
+import lombok.Data;
+
+/**
+ * @author t.miyazawa 一人一人の隊員の判定結果のDTO
+ */
+@Data
 public class MemberJudgeResponseDto {
-  private String memberName;
-  private boolean enlistedPropriety;
-
-  public String getMemberName() {
-    return memberName;
-  }
-
-  public void setMemberName(String memberName) {
-    this.memberName = memberName;
-  }
-
-  public boolean getEnlistedPropriety() {
-    return enlistedPropriety;
-  }
-
-  public void setEnlistedPropriety(boolean enlistedPropriety) {
-    this.enlistedPropriety = enlistedPropriety;
-  }
+	private String memberName;
+	private boolean enlistedPropriety;
 }
